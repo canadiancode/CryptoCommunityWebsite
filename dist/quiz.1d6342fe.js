51fa6d842b06644cc8c308454597216c55059f2d
KevinHeidema <45132144+canadiancode@users.noreply.github.com>
2023-02-04 14:07:04 -0800

    Cleaned up the learn 2 earn quiz

--- dist/quiz.1d6342fe.js
+++ dist/quiz.1d6342fe.js
@@ -172,7 +172,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../assets/PerformanteLogo.png":[["PerformanteLogo.f3be1eb1.png","assets/PerformanteLogo.png"],"assets/PerformanteLogo.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
